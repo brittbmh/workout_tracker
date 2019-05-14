679b3f91123f1f753b36e15dfdd158c66f3fc09e
Britt Carter <brittbmh@gmail.com>
2019-05-14 10:34:43 -0500

    added radio buttons

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,10 +5,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
+        <h2>
           Workout Tracker
-        </h1>
+        </h2>
       </header>
+      <div>
+        <h3>
+          Did you workout today?
+        </h3>
+        <input type="radio" id="yes-button"/>
+        <label for="yes-button">Yes</label>
+        <input type="radio" id="no-button" />
+        <label for="no-button">No</label>
+        <input type="radio" id="not-yet-button" />
+        <label for="not=yet-button">Not Yet</label>
+      </div>
     </div>
   );
 }
