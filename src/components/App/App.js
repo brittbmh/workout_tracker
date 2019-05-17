f5da671c1d2e8bab7f44b22de88730fd280c49da
Britt Carter <brittbmh@gmail.com>
2019-05-17 15:06:30 -0500

    added names to radio buttons

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -17,11 +17,9 @@ function App() {
         <h3>
           Did you workout today?
         </h3>
-        <input type="radio" id="yes-button"/>
+        <input type="radio" id="yes-button" name="workout-button"/>
         <label htmlFor="yes-button">Yes</label>
-        <input type="radio" id="no-button" />
-        <label htmlFor="no-button">No</label>
-        <input type="radio" id="not-yet-button" />
+        <input type="radio" id="not-yet-button" name="workout-button"/>
         <label htmlFor="not=yet-button">Not Yet</label>
       </div>
     </div>
