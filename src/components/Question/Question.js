816a1561440ba39e41ae382d4d8414ec2d1fa0a7
Britt Carter <brittbmh@gmail.com>
2019-05-23 11:21:55 -0500

    added styling to yesButton

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import './Question.css';
 
 class Question extends Component {
     //add conditional render to display something different if they already answered yes that day
     //switch to particle effect YES button that follows up with a random affirmative message
 
-    state = {
-        hidden: false
+    handleYes = () => {
+        //dispatch to saga to add today to database
+        //replace button with star
     }
 
     render() {
@@ -14,7 +16,7 @@ class Question extends Component {
                 <h3 id="workoutQuestion">
                     Did you workout today?
                 </h3>
-                <button id="yesButton">
+                <button id="yesButton" onClick={this.handleYes}>
                         YES!
                 </button>
                 <p><a>not yet</a></p>
